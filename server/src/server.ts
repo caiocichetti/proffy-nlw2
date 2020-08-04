3bdb4497809b8c9824a98c48344f7498c76a68ff
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-04 16:05:19 -0300

    first route :construction:

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,19 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-  return response.send('Hello World');
+  const users = [
+    { name: 'Diego', age: 25 },
+    { name: 'Vini', age: 21 },
+  ];
+  return response.send(users);
+});
+
+app.put('/users', (request, response) => {
+  const users = [
+    { name: 'Diego', age: 25 },
+    { name: 'Vini', age: 21 },
+  ];
+  return response.send(users);
 });
 
 app.listen(3333);
