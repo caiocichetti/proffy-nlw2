c5cece108b5aa1cec45cbef1dae1152b92289d09
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-04 22:04:16 -0300

    update file  :construction:

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,15 +5,24 @@ import db from './database/connection';
 const routes = express.Router();
 
 routes.post('/classes', async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
+  const { name, avatar, whatsapp, bio, subject, cost } = request.body;
 
-  await db('users').insert({
+  const insertedUserIds = await db('users').insert({
     name,
     avatar,
     whatsapp,
     bio,
   });
 
+  const user_id = insertedUserIds[0];
+
+  const insertedClassesIds = await db('classes').insert({
+    subject,
+    cost,
+    user_id,
+  });
+
+  const class_id = insertedClassesIds[0];
   return response.send();
 });
 
