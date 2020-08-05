db7f411d759b3d97cabd2dfd82305104de6744e4
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-04 23:33:24 -0300

    created a logic to create and list connections :construction:

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionController';
 
 const routes = express.Router();
 
 const classesControllers = new ClassesController();
+const connectionController = new ConnectionsController();
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionController.index);
+routes.post('/connections', connectionController.create);
 
 export default routes;
