039b96163a5392336041436154efd6ed4b78b3ed
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-06 02:47:32 -0300

    web finished :construction:

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,37 +3,50 @@ import React from 'react';
 import whatsapp from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars0.githubusercontent.com/u/58996814?s=460&u=c5d7330943e346a096fb12f7424ae5f89fda4376&v=4"
-          alt="Caio Cichetti"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Caio Cichetti</strong>
-          <span>Algoritmo</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias e computadores.
-        <br />
-        <br />
-        Apaixonado por node, java, c# e computadores gamers.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>{teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsapp} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
