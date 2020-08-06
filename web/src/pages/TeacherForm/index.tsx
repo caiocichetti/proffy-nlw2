d07aa84bab662356b4e540b39db5c97ba6a0dfea
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-05 23:25:34 -0300

    created component select :construction:

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
@@ -21,6 +22,7 @@ const TeacherForm: React.FC = () => {
           <Input name="name" label="Nome Completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
