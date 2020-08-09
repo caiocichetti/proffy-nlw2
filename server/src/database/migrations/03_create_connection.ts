082629f0a9732ce1fbd67a71f39721454e44779b
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-09 16:12:35 -0300

    end-to-end proffy application completed :heavy_check_mark:

--- server/src/database/migrations/03_create_connection.ts
+++ server/src/database/migrations/03_create_connection.ts
@@ -4,16 +4,14 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('connections', table => {
     table.increments('id').primary();
 
-    table
-      .integer('user_id')
+    table.integer('user_id')
       .notNullable()
       .references('id')
       .inTable('users')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
 
-    table
-      .timestamp('created_at')
+    table.timestamp('created_at')
       .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
       .notNullable();
   });
