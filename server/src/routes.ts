6e94945936d9cc37bde0fdb466224716659e52f4
Caio Cichetti <caio_cichetti@outlook.com>
2020-08-09 16:12:32 -0300

    end-to-end proffy application completed :heavy_check_mark:

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,17 +1,16 @@
 import express from 'express';
 
 import ClassesController from './controllers/ClassesController';
-import ConnectionsController from './controllers/ConnectionController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-const classesControllers = new ClassesController();
-const connectionController = new ConnectionsController();
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-routes.get('/classes', classesControllers.index);
-routes.post('/classes', classesControllers.create);
-
-routes.get('/connections', connectionController.index);
-routes.post('/connections', connectionController.create);
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
